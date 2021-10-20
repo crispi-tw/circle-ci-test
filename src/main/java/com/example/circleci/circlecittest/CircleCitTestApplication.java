a49ab45d77dacb254c9607c250349d1ec51cbea9
LAcristian.vidal <lacristian.vidal@LAcristianvidals-MacBook-Pro.local>
2021-10-20 19:29:17 -0300

    base proyect

---
+++ src/main/java/com/example/circleci/circlecittest/CircleCitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.circleci.circlecittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleCitTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/circleci/circlecittest/CircleCitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circleci.circlecittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
